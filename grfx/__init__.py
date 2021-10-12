30eea88bb0de89d426ec14ca7689c87204397c7f
yeongmin jo <>
2021-10-12 10:57:32 +0000

    No module name error

---
+++ grfx/__init__.py
@@ -0,0 +1,6 @@
+from globals import DEBUG
+
+#from drawer import draw
+
+if DEBUG:
+  print("== GRFX 로딩 완료 ==")
---
+++ grfx/drawer.py
@@ -0,0 +1,8 @@
+
+
+def draw(hight: int, coordinates, type: str = '.', border: bool = True, filler: str = ' '):
+	for s in coordinates:
+		for a, b in s:
+			print(a, b)
+	
+	pass
